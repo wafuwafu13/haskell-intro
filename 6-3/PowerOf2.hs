c057238da71a5731f270d1d97cc23ee7887988dc
wafuwafu13 <mariobaske@i.softbank.jp>
2022-09-23 14:25:55 +0900

    p.327

--- 6-3/PowerOf2.hs
+++ 6-3/PowerOf2.hs
@@ -1,6 +1,32 @@
-module PowerOf2 where
+module PowerOf2 
+  ( PowerOf2
+  , makePowerOf2
+  , exponentPowerOf2
+  ) where
 
 newtype PowerOf2 = PowerOf2 Integer deriving (Eq, Show)
+
+isPowerOf2 :: Integer -> Bool
+isPowerOf2 n
+  | n == 1 = True
+  | n < 1 || odd n = False
+  | otherwise = isPowerOf2 (n `div` 2)
+
+-- | 整数値から2の冪乗型への変換
+--
+-- >>> makePowerOf2 1
+-- Just (PowerOf2 1)
+-- >>> makePowerOf2 2
+-- Just (PowerOf2 2)
+-- >>> makePowerOf2 3
+-- Nothing
+-- >>> makePowerOf2 1024
+-- Just (PowerOf2 1024)
+
+makePowerOf2 :: Integer -> Maybe PowerOf2
+makePowerOf2 n
+  | isPowerOf2 n = Just (PowerOf2 n)
+  | otherwise    = Nothing
 
 -- | 2の冪乗に対し、それが2の何乗されたものかを得る
 --
@@ -18,5 +44,4 @@ exponentPowerOf2 (PowerOf2 n) = exponentPowerOf2' 0 n where
   exponentPowerOf2' :: Integer -> Integer -> Integer
   exponentPowerOf2' r n
     | n == 1         = r
-    | n < 1 || odd n = error (shows n " must be a power of 2.")
     | otherwise      = exponentPowerOf2' (r + 1) (n `div` 2)
