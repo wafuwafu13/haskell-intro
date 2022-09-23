56c9f1330925e28e421a587cdd9856ee5aff1493
wafuwafu13 <mariobaske@i.softbank.jp>
2022-09-23 10:58:50 +0900

    p.303

--- 6-2/RLE.hs
+++ 6-2/RLE.hs
@@ -16,13 +16,7 @@ rle :: String -> String
 rle = fromCharAndRunLength . toCharAndRunLength
 
 fromCharAndRunLength :: [(Char, Int)] -> String
-fromCharAndRunLength = concat . rls2strs
-
-rls2strs :: [(Char, Int)] -> [String]
-rls2strs = foreach rls2str
-
-foreach :: (a -> b) -> [a] -> [b]
-foreach = undefined
+fromCharAndRunLength = concat . map rl2str
 
 rl2str :: (Char, Int) -> String
 rl2str = undefined
