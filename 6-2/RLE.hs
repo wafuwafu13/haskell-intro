83d945809181ef25e4539ea528047b5bb687bdca
wafuwafu13 <mariobaske@i.softbank.jp>
2022-09-23 10:43:10 +0900

    p.300

--- 6-2/RLE.hs
+++ 6-2/RLE.hs
@@ -13,4 +13,10 @@ import Data.List ( group )
 --
 
 rle :: String -> String
-rle = concatMap (\s -> head s : show (length s)) . group
+rle = fromCharAndRunLength . toCharAndRunLength
+
+fromCharAndRunLength :: [(Char, Int)] -> String
+fromCharAndRunLength = undefined
+
+toCharAndRunLength :: String -> [(Char, Int)]
+toCharAndRunLength = undefined
