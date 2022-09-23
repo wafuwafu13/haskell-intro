529caa52122f6e98a06a1fba2153ed672ce0283d
wafuwafu13 <mariobaske@i.softbank.jp>
2022-09-23 10:35:02 +0900

    p.299

--- 6-2/RLE.hs
+++ 6-2/RLE.hs
@@ -1,5 +1,7 @@
 module RLE where
 
+import Data.List ( group )
+
 -- | ランレングス圧縮
 --
 -- >>> rle ""
@@ -11,10 +13,4 @@ module RLE where
 --
 
 rle :: String -> String
-rle "" = ""
-rle (h:t) = aux 1 h t where
-  aux :: Int -> Char -> String -> String
-  aux runLength prevChar "" = prevChar : show runLength
-  aux runLength prevChar (c:s)
-    | c == prevChar = aux (runLength + 1) prevChar s
-    | otherwise     = prevChar : shows runLength (aux 1 c s)
+rle = concatMap (\s -> head s : show (length s)) . group
