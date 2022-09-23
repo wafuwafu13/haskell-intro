205528e64c617e6680c57b09dbed5a9575e8e466
wafuwafu13 <mariobaske@i.softbank.jp>
2022-09-23 10:31:23 +0900

    p.299

--- 6-2/RLE.hs
+++ 6-2/RLE.hs
@@ -11,4 +11,10 @@ module RLE where
 --
 
 rle :: String -> String
-rle = undefined
+rle "" = ""
+rle (h:t) = aux 1 h t where
+  aux :: Int -> Char -> String -> String
+  aux runLength prevChar "" = prevChar : show runLength
+  aux runLength prevChar (c:s)
+    | c == prevChar = aux (runLength + 1) prevChar s
+    | otherwise     = prevChar : shows runLength (aux 1 c s)
